3847d143d00169a5eee615d0b6cac392c75bbb4d
Thomas Kirz <thomas.kirz@gmail.com>
2018-01-29 21:43:21 +0100

    Add function to derive polynomials

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 func main() {
 	// polynomial represented as slice with it's degree n being the length of the slice - 1
 	// the first value is the coefficient of x^n, then of x^(n-1)..., the last value is a constant
-	p := []uint{3, 5, 0, 3, 5, 0, 1}
-
+	p := []uint{3, 5, 0, 3, 5, 2, 1}
 	printPolynomial(p)
+
+	d := derive(p)
+	fmt.Println("Derivative:")
+	printPolynomial(d)
 }
 
 func printPolynomial(p []uint) {
@@ -30,4 +33,16 @@ func printPolynomial(p []uint) {
 	}
 	fmt.Println()
 	return
+}
+
+func derive(function []uint) []uint {
+	degree := uint(len(function) - 1) // degree of derivative
+	derivative := make([]uint, degree)
+
+	for i := range derivative {
+		derivative[i] = function[i] * degree
+		degree--
+	}
+
+	return derivative
 }
