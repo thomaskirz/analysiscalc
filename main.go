85c61da4df4eae56c54ad85facfaa585cb26ae76
Thomas Kirz <thomas.kirz@gmail.com>
2018-02-20 21:29:33 +0100

    Change data type of polynomial

--- main.go
+++ main.go
@@ -4,45 +4,78 @@ import (
 	"fmt"
 )
 
-func main() {
-	// polynomial represented as slice with it's degree n being the length of the slice - 1
-	// the first value is the coefficient of x^n, then of x^(n-1)..., the last value is a constant
-	p := []uint{3, 5, 0, 3, 5, 2, 1}
-	printPolynomial(p)
-
-	d := derive(p)
-	fmt.Println("Derivative:")
-	printPolynomial(d)
+// Polynomial represents a polynomial function
+type Polynomial struct {
+	Degree   uint
+	Summands map[uint]float64
 }
 
-func printPolynomial(p []uint) {
-	degree := len(p) - 1
-	for _, v := range p {
-		if v != 0 {
-			if degree == len(p)-1 {
-				fmt.Printf("%dx^%d", v, degree)
-			} else if degree > 1 {
-				fmt.Printf(" + %dx^%d", v, degree)
-			} else if degree == 1 {
-				fmt.Printf(" + %dx", v)
+func main() {
+	p := Polynomial{
+		6,
+		map[uint]float64{
+			0: -1,
+			1: 2,
+			2: -5,
+			3: 33,
+			5: 5,
+			6: -3,
+		},
+	}
+
+	printPolynomial(p)
+}
+
+func printPolynomial(p Polynomial) {
+	s := p.Summands
+	deg := p.Degree
+
+	// iterate down from highest (degree) to lowest (0) coefficient
+	for i := deg; ; i-- {
+		if s[i] > 0 {
+			// positive coefficient
+			if i < deg {
+				fmt.Printf(" + ")
+			}
+			if i > 1 {
+				fmt.Printf("%gx^%d", s[i], i)
+			} else if i == 1 {
+				fmt.Printf("%gx", s[i])
 			} else {
-				fmt.Printf(" + %d", v)
+				fmt.Printf("%g", s[i])
+			}
+		} else if s[i] < 0 {
+			// negative coefficient
+			if i < deg {
+				fmt.Printf(" - ")
+			} else {
+				fmt.Printf("-")
+			}
+			if i > 1 {
+				fmt.Printf("%gx^%d", s[i]*-1, i)
+			} else if i == 1 {
+				fmt.Printf("%gx", s[i]*-1)
+			} else {
+				fmt.Printf("%g", s[i]*-1)
 			}
 		}
-		degree--
+		if i == 0 {
+			break
+		}
 	}
 	fmt.Println()
-	return
 }
 
 func derive(function []uint) []uint {
-	degree := uint(len(function) - 1) // degree of function
-	derivative := make([]uint, degree)
+	// TODO
 
-	for i := range derivative {
-		derivative[i] = function[i] * degree
-		degree--
-	}
+	// degree := uint(len(function) - 1) // degree of function
+	// derivative := make([]uint, degree)
 
-	return derivative
+	// for i := range derivative {
+	// 	derivative[i] = function[i] * degree
+	// 	degree--
+	// }
+
+	// return derivative
 }
