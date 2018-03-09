0ab695fdad1832f62d3cc2ed610f53615e00a5f6
Thomas Kirz <thomas.kirz@gmail.com>
2018-03-09 12:07:38 +0100

    implement zero calculator for functions with degree greater than 2 if the function or any of its derivatives has a saddle point, calculation doesn't work yet

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/tombom4/analysiscalc/polynomial"
-	"os"
 )
 
 func main() {
-	functions := make(polynomial.Functions)
+	//functions := make(polynomial.Functions)
+	//
+	//reader := bufio.NewReader(os.Stdin)
+	//for {
+	//	fmt.Print("Waiting for function: ")
+	//	text, _ := reader.ReadString('\n')
+	//	fmt.Println(functions.Evaluate(text))
+	//}
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("Waiting for function: ")
-		text, _ := reader.ReadString('\n')
-		fmt.Println(functions.Evaluate(text))
+	p := polynomial.Polynomial{
+		0: 1,
+		1: 1,
+		2: -3,
+		3: 0,
+		4: 1,
 	}
+
+	zeroes, _ := p.Zeroes(0.001)
+	for _, v := range zeroes {
+		fmt.Printf("%.3f\t", v)
+	}
+	fmt.Println()
 }
--- polynomial/polynomial.go
+++ polynomial/polynomial.go
@@ -79,22 +79,100 @@ func (p Polynomial) Derive() Polynomial {
 }
 
 // Zeroes returns a slice containing the zeroes of a quadratic of linear function in no specific order.
-func (p Polynomial) Zeroes() []float64 {
+func (p Polynomial) Zeroes(accuracy float64) ([]float64, error) {
+
+	// TODO: handle saddle points
 
 	if p.Degree() == 1 {
-		return []float64{-(p[1] / p[0])} // ax+b=0 => x=-a/b
+		return []float64{-(p[1] / p[0])}, nil // ax+b=0 => x=-a/b
 	} else if p.Degree() == 2 {
+
 		// solve equation with quadratic formula
 		discriminant := p[1]*p[1] - (4 * p[2] * p[0]) // determines amount of zeroes
 		if discriminant == 0 {
 			// the function has one zero
-			return []float64{-p[1] / 2 * p[2]}
+			return []float64{-p[1] / 2 * p[2]}, nil
 		} else if discriminant > 0 {
 			x1 := (-p[1] - math.Sqrt(discriminant)) / (2 * p[2])
 			x2 := (-p[1] + math.Sqrt(discriminant)) / (2 * p[2])
-			return []float64{x1, x2}
+			return []float64{x1, x2}, nil
+		} else {
+			return []float64{}, nil
 		}
+
+	} else {
+		extrema, err := p.Extrema(accuracy)
+		if err != nil {
+			return nil, err
+		}
+
+		zeroes := make([]float64, 0)
+
+		// presign and postsign signify if the limit of p is plus or minus infinity (y) at plus and minus infinity (x)
+		presign := (p.Degree()%2 == 0 && p[p.Degree()] > 0) || (p.Degree()%2 == 1 && p[p.Degree()] < 0)
+		postsign := (p.Degree()%2 == 0 && p[p.Degree()] > 0) || (p.Degree()%2 == 1 && p[p.Degree()] > 0)
+
+		fmt.Printf("extrema: %v\n", extrema)
+
+		for i, extremum := range extrema {
+			// if this is the first extremum and there is a zero before it, calculate it with Newton's method
+			// if this is the last extremum and there is a zero after it, calculate it with Newton's method
+			// otherwise if there is a zero between this and the next extremum, calculate it with Newton's method
+			if i == 0 {
+				if !math.Signbit(p.Valueat(extremum)) != presign {
+					if nwtn, err := p.newton(extremum-1, accuracy); err != nil {
+						return nil, err
+					} else {
+						zeroes = append(zeroes, nwtn)
+						fmt.Printf("pre    : i: %v, extremum: %v, zero: %v\n", i, extremum, nwtn)
+					}
+				}
+			}
+			if i == len(extrema)-1 {
+				if !math.Signbit(p.Valueat(extremum)) != postsign {
+					if nwtn, err := p.newton(extremum+1, accuracy); err != nil {
+						return nil, err
+					} else {
+						zeroes = append(zeroes, nwtn)
+						fmt.Printf("post   : i: %v, extremum: %v, zero: %v\n", i, extremum, nwtn)
+					}
+				}
+				continue
+			}
+			if math.Signbit(p.Valueat(extremum)) != math.Signbit(p.Valueat(extrema[i+1])) {
+				if nwtn, err := p.newton((extremum+extrema[i+1])/2, accuracy); err != nil {
+					return nil, err
+				} else {
+					zeroes = append(zeroes, nwtn)
+					fmt.Printf("between: i: %v, extremum: %v, zero: %v\n", i, extremum, nwtn)
+				}
+			}
+		}
+
+		return zeroes, nil
 	}
 
-	return nil
+}
+
+func (p Polynomial) Extrema(accuracy float64) ([]float64, error) {
+	return p.Derive().Zeroes(accuracy)
+}
+
+func (p Polynomial) Valueat(x float64) (y float64) {
+	for exp, coeff := range p {
+		y += coeff * math.Pow(x, float64(exp))
+	}
+	return
+}
+
+func (p Polynomial) newton(x float64, e float64) (float64, error) {
+	n, m := 0., x
+	for i := 0; i < 1000; i++ {
+		n = m
+		m = n - (p.Valueat(n) / p.Derive().Valueat(n))
+		if math.Abs(m-n) < e {
+			return m, nil
+		}
+	}
+	return 0, fmt.Errorf("result not converging, starting at %v", x)
 }
